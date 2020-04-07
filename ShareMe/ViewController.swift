e35a8bbf58a96e3b92cb4a79c6dbdca6c0c1ada0
SRihet <stephane.rihet@gmail.com>
2020-04-07 18:10:41 +0200

    Initial Commit

---
+++ ShareMe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShareMe
+//
+//  Created by Stéphane Rihet on 07/04/2020.
+//  Copyright © 2020 SapinRocher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
